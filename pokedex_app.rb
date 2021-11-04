74434fb1b04c5bb65ddc755e2a91cbc038d2b3d1
Jayakrishnan M K <jayakrishnan.mk@pairfinance.com>
2021-11-04 13:03:39 +0100

    Display random pokemon name and image on index page

--- pokedex_app.rb
+++ pokedex_app.rb
@@ -1,14 +1,20 @@
+# frozen_string_literal: true
+
+require_relative './services/pokemon_details_fetcher'
+
+# Pokedox Sinatra APP
 class PokedexApp < Sinatra::Application
   enable :sessions
 
   get '/' do
+    @pokemon = PokemonDetailsFetcher.fetch_random_pokemon_data
     display_page :index
   end
 
   def display_page(location, locals = {})
     options = {
       layout_options: { views: './views/layouts' },
-      layout: locals.fetch(:layout) { :default },
+      layout: locals.fetch(:layout, :default),
       locals: locals
     }
 
